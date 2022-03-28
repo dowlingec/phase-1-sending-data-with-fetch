6b8e7b395fcb0f097324a339f017b2a01a1bb4d9
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-28 17:12:58 -0400

    commit tree 61bb7d3e739357e5ab2105ae818c10a4efa721a9

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your code here
+
+const sumbitData = (userInput) => {
+    
+    fetch('http://localhost:3000/users',{method: 'POST',
+        headers: {
+        "Content-Type" : "application/json",
+        "Accept" : "application/json"},
+        body:JSON.stringify(userInput)
+    })
+}
+
+sumbitData()
